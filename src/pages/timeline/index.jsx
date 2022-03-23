bbd61ff734f232b8082950c5498de282d3f01b96
rodnei0 <rodnei0@gmail.com>
2022-03-23 16:13:18 -0400

    working in front-end

--- src/pages/timeline/index.jsx
+++ src/pages/timeline/index.jsx
@@ -1,18 +1,36 @@
-import { Container } from './styles'
+import { useState } from 'react'
+import { Container, CreatePost, Link, Message, Form, Button } from './styles'
 
 import Topbar from '../../components/Topbar/Topbar'
 
 
 function Timeline() {
+	const [link, setLink] = useState('')
+	const [message, setmessage] = useState('')
+
 	return (
 		<Container>
-			<Topbar/>
+			<CreatePost>
+				<div>What are you going to share today?</div>
+				<Form onSubmit={() => {}}>
+					<Link 
+						type="url" 
+						value={link} 
+						onChange={(e) => setLink(e.target.value)} 
+						placeholder='http://...'>
+					</Link>
+					<Message 
+						type="text" 
+						value={message}
+						onChange={(e) => setmessage(e.target.value)} 
+						placeholder='Awesome article about #javascript'>
+					</Message>
+					<Button type='submit' >Publish</Button>
+				</Form>
+			</CreatePost>
 		</Container>
 	)
 }
 
 
 export default Timeline
-
-
-
