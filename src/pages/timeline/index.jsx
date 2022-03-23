b6d2e9213b93257cac21e08e7f27ebedd25209cf
rodnei0 <rodnei0@gmail.com>
2022-03-23 16:13:18 -0400

    working in front-end

--- src/pages/timeline/index.jsx
+++ src/pages/timeline/index.jsx
@@ -1,16 +1,34 @@
-import { Container } from './styles'
+import { useState } from 'react'
+import { Container, CreatePost, Link, Message, Form, Button } from './styles'
 
 
 function Timeline() {
+	const [link, setLink] = useState('')
+	const [message, setmessage] = useState('')
+
 	return (
 		<Container>
-			
+			<CreatePost>
+				<div>What are you going to share today?</div>
+				<Form onSubmit={() => {}}>
+					<Link 
+						type="url" 
+						value={link} 
+						onChange={(e) => setLink(e.target.value)} 
+						placeholder='http://...'>
+					</Link>
+					<Message 
+						type="text" 
+						value={message}
+						onChange={(e) => setmessage(e.target.value)} 
+						placeholder='Awesome article about #javascript'>
+					</Message>
+					<Button type='submit' >Publish</Button>
+				</Form>
+			</CreatePost>
 		</Container>
 	)
 }
 
 
 export default Timeline
-
-
-
--- src/pages/timeline/styles.js
+++ src/pages/timeline/styles.js
@@ -2,10 +2,89 @@ import styled from 'styled-components'
 
 
 const Container = styled.div`
-	
+	background-color: #333333;
+	height	: 100vh;
+`
+
+const CreatePost = styled.div`
+	display: flex;
+	justify-content: center;
+	flex-wrap: wrap;
+
+	width: 100%;
+	height: 164px;
+	padding-top: 10px;
+
+	background: #FFFFFF;
+	box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+
+	div {
+		font-family: 'Lato', sans-serif;
+		font-weight: 300;
+		font-size: 17px;
+		line-height: 20px;
+		color: #707070;
+
+		height: fit-content;
+	}
+`
+const Form = styled.form`
+	display: flex;
+	flex-direction: column;
+	align-items: flex-end;
+
+	gap: 5px;
+    width: 100%;
+	padding-right: 15px;
+`
+
+const Link = styled.input`
+	width: calc(100% - 30px);
+	height: 30px;
+
+	background: #EFEFEF;
+	border-radius: 5px;
+
+	::placeholder {
+		font-family: 'Lato';
+		font-style: normal;
+		font-weight: 300;
+		font-size: 13px;
+		line-height: 16px;
+		color: #949494;
+
+		padding-left: 11px;
+	}
+`
+
+const Message = styled.input`
+	width: calc(100% - 30px);
+	height: 47px;
+
+	background: #EFEFEF;
+	border-radius: 5px;
+
+	::placeholder {
+		font-family: 'Lato';
+		font-style: normal;
+		font-weight: 300;
+		font-size: 13px;
+		line-height: 16px;
+		color: #949494;
+
+		padding-left: 11px;
+	}
+`
+
+const Button = styled.button`
+	width: 112px;
+	height: 22px;
+
+	background: #1877F2;
+	border-radius: 5px;
 `
 
 
 export {
-	Container
+	Container, CreatePost, Link, Message, Form, Button
 }
